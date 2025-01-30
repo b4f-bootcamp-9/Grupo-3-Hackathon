6763fe883fdba642eba6b72d6218db3a0cc2d15c
SrCOBOL <kikobborges@gmail.com>
2025-01-30 11:47:43 +0000

    Limpei codigo antigo

--- src/server/models/house.js
+++ src/server/models/house.js
@@ -1,26 +1,4 @@
 import mongoose from "mongoose";
-
-// const casaSchema = new mongoose.Schema(
-//   {
-//     house_name: String,
-//     distrito: String,
-//     municipio: String,
-//     tipo_habitacao: String,
-//     descricao: String,
-//     orcamento: String,
-//     quartos: Number,
-//     casa_banho: Number,
-//     varanda: Boolean,
-//     piscina: Boolean,
-//     area_ext: Boolean,
-//     garagem: Boolean,
-//     ano_constr: Number,
-//   },
-//   {
-//     collection: "casas",
-//   }
-// );
-
 const casaSchema = new mongoose.Schema(
   {
     house_name: { type: String },
